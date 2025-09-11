5565fedfa89e61de6506d1a5055688f3e6bbe4c5
kazinayeem <mohammadalinayeem360@gmail.com>
2025-09-11 19:11:16 +0600

    solve cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,14 +9,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "https://cafe-sync.vercel.app/",
+  })
+);
 app.use(express.json());
 
 // Routes
 app.get("/", (req: Request, res: Response) => {
   res.send("☕ Cafe POS Server is running!");
 });
-
 
 app.use("/api/users", userRoutes);
 // Example route for testing errors
